8f6642f4c2779f4ac4c82acd952aeba03dec9c7b
Dragos Velicanu <dragosvelicanu@gmail.com>
2023-12-21 12:01:55 -0500

    pdf merger

--- src/app.py
+++ src/app.py
@@ -1,8 +1,24 @@
 import streamlit as st
+from pikepdf import Pdf
 
 
 def main():
-    st.write("Hello World!")
+    files = st.file_uploader("Choose files", type="pdf", accept_multiple_files=True)
+    new = Pdf.new()
+    for file_ in files:
+        with Pdf.open(file_) as pdf:
+            for page in pdf.pages:
+                new.pages.append(page)
+        print(file_)
+    new.save("bar.pdf")
+    with open("bar.pdf", "rb") as f:
+        btn = st.download_button(
+            label="Download merged pdf",
+            data=f,
+            file_name="merged.pdf",
+            mime="application/pdf",
+        )
+
     return "done"
 
 
